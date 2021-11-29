078f7a688cdd6840b3952a43314a27c1cee2f99f
donarturo <arturwrona91@gmail.com>
2021-11-29 23:24:38 +0100

    Clean&Tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,36 @@
-#include <QApplication>
 #include <vector>
-#include "window.h"
+#include "globals.h"
+
 #include "AudioEngine.h"
 #include "WaveSimple.h"
 #include "WaveSimpleMixer.h"
-#include "globals.h"
-//class WaveSimpleMixer;
-class AudioEngine;
+
+#include <QApplication>
+#include "window.h"
+
 int main(int argc,  char **argv)
 {
-	QApplication app(argc, argv);
-    Window window;
-        
+    stk::Stk::showWarnings( false );
+    stk::Stk::printErrors( false );
+
     std::vector <stk::WaveSimple*> synth;
     
-	AudioEngine audioengine;
+    AudioEngine audioengine;
     stk::WaveSimpleMixer mixer;
             
     for (int i=0; i<KEY_NUMBERS; i++) {
         synth.push_back(new stk::WaveSimple);
         synth[i]->loadWave("harpsichord.wav");
         synth[i]->setBaseFrequency(220);
-        
         mixer.addInstrument(synth[i]);
-        
     }
-        
-   
+
     audioengine.connectMixer(&mixer);
     audioengine.init();
-    
+
+    QApplication app(argc, argv);
+    Window window;
     window.initKeyboard(&synth);
 	window.show();
 	return app.exec();
-	
-
 }
