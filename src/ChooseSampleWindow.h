1eb277349f2707a111bab4e740dac5a82e79d015
Artur Wrona <arturwrona91@gmail.com>
2022-05-22 19:44:37 +0200

    Write GUI basics

--- src/ChooseSampleWindow.h
+++ src/ChooseSampleWindow.h
@@ -17,18 +17,25 @@ class ChooseSampleWindow : public QDialog
 	Q_OBJECT
 	public:
 		explicit ChooseSampleWindow(QWidget *parent=0);
-        
+        void init();
         QPushButton *m_quit_btn;
         QLabel *m_label;
         
     
 	private:
         void setString();
+        QLineEdit *m_waveurl;
+        QPushButton *m_browse_btn;
+        QPushButton *m_apply_btn;
+        QPushButton *m_cancel_btn;
         
     protected:
         QString label_str="";
     
     public slots:
+        void quitSlot();
+        void browseSlot();
+        void applySlot();
         
     signals:
         
