16f5580707a84d0137db952466007bd7b585cdc7
donarturo <arturwrona91@gmail.com>
2022-06-09 23:05:20 +0200

    Improve text in About dialog

--- src/AboutWindow.cpp
+++ src/AboutWindow.cpp
@@ -5,7 +5,11 @@ AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent)
 {
     this->setAboutString();
     this->setWindowTitle("About");
-    m_label = new QLabel(label_str, this);
+    m_label = new QLabel(this);
+    m_label->setTextFormat(Qt::RichText);
+    m_label->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    m_label->setOpenExternalLinks(true);
+    m_label->setText(this->label_str);
     m_label->show();
     
     
@@ -13,20 +17,28 @@ AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent)
 //-------------------------------------------
 void AboutWindow::setAboutString()
 {
-    label_str+="TuningTrainer\n";
-    label_str+="by donarturo11\n";
-    label_str+="\n";
-    label_str+="TuningTrainer is a small and simple program\n";
-    label_str+="to training tuning systems,\n";
-    label_str+="recommended for\n";
-    label_str+="harpsichordists, organists\n";
-    label_str+="and other who interest in early music.\n";
-    label_str+="\n\n";
-    label_str+="This program uses:\n";
-    label_str+="STK Synthesis Toolkit:\n https://ccrma.stanford.edu/software/stk/ \n\n";
-    label_str+="Realtime Audio:\n https://www.music.mcgill.ca/~gary/rtaudio/ \n\n";
-    label_str+="QT5:\n https://www.qt.io/ \n\n";
-    label_str+="\n\n";
-    label_str+="To contact me, please write at address\n";
-    label_str+="arturwrona91(at)gmail(dot)com\n";
+    QString mailaddress="arturwrona91\x40gmail.com";
+    QString mailtoString="mailto:" + mailaddress + "\\?subject=tuningTrainer Support";
+    
+    label_str+="<h1>TuningTrainer</h1><br>";
+    label_str+="by donarturo11<br>";
+    label_str+="<a href=\"https://github.com/donarturo11/tuningTrainer\">https://github.com/donarturo11/tuningTrainer</a><br><br>";
+    label_str+="TuningTrainer is a small and simple program<br>";
+    label_str+="to training tuning systems,<br>";
+    label_str+="recommended for<br>";
+    label_str+="harpsichordists, organists<br>";
+    label_str+="and other who interest in early music.<br>";
+    label_str+="<br><br>";
+    label_str+="This program uses:<br>";
+    label_str+="<b>STK Synthesis Toolkit:</b><br>";
+    label_str+="<a href=\"https://ccrma.stanford.edu/software/stk/\">https://ccrma.stanford.edu/software/stk/</a><br><br>";
+    label_str+="<b>Realtime Audio:</b><br>";
+    label_str+="<a href=\"https://www.music.mcgill.ca/~gary/rtaudio/\">https://www.music.mcgill.ca/~gary/rtaudio/</a><br><br>";
+    label_str+="<b>QT5:</b><br>";
+    label_str+="<a href=\"https://www.qt.io/\">https://www.qt.io/</a>";
+    label_str+="<br><br>";
+    label_str+="To contact me, please write me at address:<br>";
+    label_str+="<a href=\"" + mailtoString + "\">"+mailaddress+"</a><br><br>";
+    label_str+="To close this window press &lt;esc&gt;";
+    
 }
