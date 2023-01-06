db37c82227ff920c4b0138734378ad75a7535e3a
Artur Wrona <arturwrona91@gmail.com>
2023-01-06 16:08:07 +0100

    Fix finding STK includes

--- cmake/findstk.cmake
+++ cmake/findstk.cmake
@@ -1,13 +1,12 @@
-include(FindPackageHandleStandardArgs.cmake)
-find_path(STK_INCLUDE_DIR stk/Stk.h)
-find_path(STK_INCLUDE_DIR stk/stk.h)
-find_library(STK_LIBRARY stk libstk.a)
+include(FindPackageHandleStandardArgs)
+find_path(STK_INCLUDE_DIR stk/Stk.h stk)
+find_library(STK_LIBRARY stk)
 
-#find_package_handle_standard_args(STK
-# DEFAULT_MSG
-#  STK_INCLUDE_DIR
-#  STK_LIBRARY
-#)
+find_package_handle_standard_args(STK
+ DEFAULT_MSG
+  STK_INCLUDE_DIR
+  STK_LIBRARY
+)
 
 mark_as_advanced(STK_LIBRARY STK_INCLUDE_DIR)
 
