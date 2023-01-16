0db124019c68c891c74164088a3fe3c718cf79e1
Artur Wrona <arturwrona91@gmail.com>
2023-01-16 11:30:34 +0100

    Remove uneeded things

--- src/gui/controllerwidget/Keyboard.cpp
+++ src/gui/controllerwidget/Keyboard.cpp
@@ -1,14 +1,11 @@
 #include "gui/controllerwidget/Keyboard.h"
 #include "gui/controllerwidget/KeyButton.h"
-#include "gui/load_style.h"
+
 namespace GUI {
 /* --------------- */
 Keyboard::Keyboard(QWidget *parent) : QWidget(parent)
 {
-    GUI::load_style(this, ":/qss/keyboard.qss");
-    for (bool s : {0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0}) {
-        initKey(s);
-    }
+
 }
 
 Keyboard::~Keyboard()
@@ -21,7 +18,6 @@ void Keyboard::initKey(bool semitone)
     keys.push_back(new KeyButton(this, semitone));
     connect(keys[idx], &KeyButton::noteOn, this, &Keyboard::keyPressed );
     connect(keys[idx], &KeyButton::noteOff, this, &Keyboard::keyReleased );
-    KeyButton::index++;
 }
 
 void Keyboard::keyPressed(int index)
