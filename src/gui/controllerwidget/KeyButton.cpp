b40829d9a3f53cd8fe6ae4919969ae65b4f7fde0
Artur Wrona <arturwrona91@gmail.com>
2023-01-16 11:23:51 +0100

    KeyButton: Clear debug message for contructor

--- src/gui/controllerwidget/KeyButton.cpp
+++ src/gui/controllerwidget/KeyButton.cpp
@@ -12,15 +12,10 @@ KeyButton::KeyButton(QWidget *parent, bool semitone) : QPushButton(parent)
     //setAutoRepeat(1);
     setSemitone(semitone);
     init();
-    fprintf(stderr, "\n\n==========================\n");
-    fprintf(stderr, "-- Keybutton C-tor ------\n");
-    fprintf(stderr, "-- Created Key number: %i\n", btn_index);
-    fprintf(stderr, "-- Pos X: %i\n", pos_x);
-    qDebug() << "Geometry: " << geometry();
-    qDebug() << "Is semitone: " << isSemitone();
-    fprintf(stderr, "\n==========================\n");
     connect(this, SIGNAL(pressed()), this, SLOT(sendNoteOn()));
     connect(this, SIGNAL(released()), this, SLOT(sendNoteOff()));
+    qDebug() << "Created KeyButton at " << KeyButton::index;
+    KeyButton::index++;
     
 }
 
