60593f6eed6a21720caea4d9f7fe944a1e732143
Artur Wrona <arturwrona91@gmail.com>
2023-01-17 00:46:37 +0100

    Add access to MainWindow from children instances

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -4,11 +4,13 @@
 #include <QApplication>
 #include <QMainWindow>
 #include <QDebug>
+#include <QKeyEvent>
 #include <QSettings>
 #include <QHBoxLayout>
 
 #include "gui/MainButtons.h"
 #include "gui/ControllerWidget.h"
+#include "gui/KeyEvents.h"
 
 #include "globals.h"
 #include "semitones.h"
@@ -24,10 +26,14 @@ class MainWindow : public QMainWindow
 	public:
 		MainWindow();
 		~MainWindow();
+        static MainWindow* getMainWindow(){ return MainWindow::mainWindow; }
     private:
+        inline static MainWindow* mainWindow=0;
         void init();
         void initSettings();
         void setDefaults();
+        void keyPressEvent(QKeyEvent *event);
+        void keyReleaseEvent(QKeyEvent *event);
         
     protected:
         int waitCount;
@@ -38,10 +44,13 @@ class MainWindow : public QMainWindow
         QSettings *m_settings;
         QHBoxLayout* m_layout;
         QWidget* mainwidget;
+        GUI::KeyEvents *keyEvents; 
     
     public slots:
         
     signals:
+        void keyPressed(int keyCode);
+        void keyReleased(int keyCode);
 
 	private slots:
         void quitSlot();
