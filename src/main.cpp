1fa05761eaeaa23de52a67699f3a7d96e249dc53
Artur Wrona <arturwrona91@gmail.com>
2023-01-26 09:19:31 +0100

    Tidy up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,36 +15,8 @@
 
 int main(int argc,  char **argv)
 {
-/*
-#ifdef DEBUG
-    fprintf(stderr, "tuningTrainer - Debug mode");
-    qSetMessagePattern("%{function} | %{message}");
-#endif
-#ifdef _STK_DEBUG_
-    stk::Stk::showWarnings( true );
-    stk::Stk::printErrors( true );
-#else
-    stk::Stk::showWarnings( false );
-    stk::Stk::printErrors( false );
-#endif
-
-    std::vector <stk::WaveSimple*> synth;
-    
-    AudioEngine audioengine;
-    stk::WaveSimpleMixer mixer;
-    
-    for (int i=0; i<KEY_NUMBERS; i++) {
-        synth.push_back(new stk::WaveSimple);
-        synth[i]->setBaseFrequency(220);
-        mixer.addInstrument(synth[i]);
-    }
-
-    audioengine.connectMixer(&mixer);
-    audioengine.init();
-*/
     QApplication app(argc, argv);
     MainWindow mainwindow;
-    //mainwindow.initKeyboard(&synth);
 	mainwindow.show();
 	return app.exec();
 }
