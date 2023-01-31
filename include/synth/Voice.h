4e76fe97ed49a93b8b368d4208c04b8ee91b7cfd
donarturo11 <arturwrona91@gmail.com>
2023-01-31 21:03:28 +0100

    Reference to wave sample instead copy

--- include/synth/Voice.h
+++ include/synth/Voice.h
@@ -1,34 +1,42 @@
 #ifndef VOICE_H
 #define VOICE_H
 #include "synth/common.h"
+#include "synth/Synthesizer.h"
 #include "stk/LentPitShift.h"
 namespace Synth {
+class Synthesizer;
 class Voice
 {
 public:
     inline static int index=0;
-    Voice();
+    Voice(Synthesizer* synth);
     ~Voice();
     void setNoteOn();
     void setNoteOff();
     void setFrequency(double frequency);
     void setBaseFrequency(double frequency){ _base_frequency = frequency; }
+    void processWave();
     float tick();
     bool noteOn() { return _noteOn; }
     int getIndex() { return _index; }
     /* Wave */
-    void loadWave(Samples wave);
+    void loadWave(float* wave);
     void resetLoop(){ _wave_offset = 0; }
     void setRate(unsigned int rate);
-    int waveSize() { return _wave.size(); }
+    int waveSize() { return _wave_size; }
+    bool waveEmpty() { return _wave_size == 0; }
+    void setWaveSize(unsigned int size) { _wave_size = size; }
 protected:
+    Synthesizer* _synth;
     int _index;
     unsigned int _wave_offset;
     unsigned int _wave_rate;
+    unsigned int _wave_size;
     bool _noteOn;
     double _frequency;
     double _base_frequency;
     stk::LentPitShift _pitchShift;
+    float* _wave_orig;
     Samples _wave;
 };
 }
